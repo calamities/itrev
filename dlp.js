53a64b5bba1a5c5d4580106f111877f6ff69eae3
calamities <33078727+calamities@users.noreply.github.com>
2020-10-22 21:24:55 -0400

    Update dlp.js

--- dlp.js
+++ dlp.js
@@ -20,7 +20,7 @@ function dlp() {
                         'videoMinutesWatched': '{{videoMinutesWatched}}'
                     });
                     */
-                }
+                });
             });
         });
     }
