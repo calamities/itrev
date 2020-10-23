32f614375f439bbea0aa29708832b5f92b55a5c0
calamities <33078727+calamities@users.noreply.github.com>
2020-10-22 21:22:41 -0400

    Update dlp.js

--- dlp.js
+++ dlp.js
@@ -1,17 +1,36 @@
 console.log("-dlp-")
+var i = 0
 function dlp() {
-    console.log('-dlp-')
+    i++
+    if (playing) {
+        player.getCurrentTime().then(function(seconds) {
+            player.getVideoTitle().then(function(title) {
+                player.getVideoID().then(function(id) {
+                    console.log(seconds) 
+                    console.log(title)
+                    console.log(id)
+                    console.log(i)
+                    /*
+                    <script>
+                    dataLayer.push({
+                        'event': 'videoPlayerEvent',
+                        'videoId': '{{VideoId}}',
+                        'videoName': '{{VideoName}}',
+                        'videoPlayerTime': '{{videoPlayerTime}}',
+                        'videoMinutesWatched': '{{videoMinutesWatched}}'
+                    });
+                    */
+                }
+            });
+        });
+    }
+    else {
+
+    } 
     setTimeout(
         function () {
-            if (playing) {
-                player.getCurrentTime().then(function(seconds) {
-                    console.log(seconds)
-                });
-            }
-            else {
-
-            } dlp();
+            dlp();
         }
-        , 60000);
+        , 15000);
 }
 dlp();
