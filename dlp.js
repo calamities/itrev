4875908b5cfe532a80952488f95f02cc6e1b1694
calamities <calamities2@gmail.com>
2020-11-13 06:44:49 -0500

    test running function regardless if playing or not

--- dlp.js
+++ dlp.js
@@ -2,44 +2,44 @@ var i = -1
 var dlp_player = new Vimeo.Player(vimeo_iframe);
 var isPlaying = false
 function dlp() {
-    i++
     if (isPlaying) {
-        dlp_player.getCurrentTime().then(function(seconds) {
-            dlp_player.getVideoTitle().then(function(title) {
-                dlp_player.getVideoId().then(function(id) {
-                    /*
-                    console.log(seconds) 
+        i++
+        dlp_player.getCurrentTime().then(function (seconds) {
+            dlp_player.getVideoTitle().then(function (title) {
+                dlp_player.getVideoId().then(function (id) {
+
+                    console.log(isPlaying)
+                    console.log(seconds)
                     console.log(title)
                     console.log(id)
                     console.log(i)
-                    */
-                    dataLayer.push({
-                        'event': 'videoPlayerEvent',
-                        'videoId': id,
-                        'videoName': title,
-                        'videoPlayerTime': seconds,
-                        'videoMinutesWatched': i
-                    });
-                    console.log("dataLayer push");
+
+                    /*
+                      dataLayer.push({
+                          'event': 'videoPlayerEvent',
+                          'videoId': id,
+                          'videoName': title,
+                          'videoPlayerTime': seconds,
+                          'videoMinutesWatched': i
+                      });
+                      */
                 });
             });
         });
+    }
     setTimeout(
-    function () {
-        dlp();
-    }
-    , 60000);
-    }
+        function () {
+            dlp();
+        }
+        , 60000);
 }
 dlp_player.on('play', function () {
-    isPlaying=true
+    isPlaying = true
     dlp();
-}); 
+});
 dlp_player.on('pause', function () {
-    isPlaying=false
-    dlp();
-}); 
+    isPlaying = false
+});
 dlp_player.on('ended', function () {
-    isPlaying=false
-    dlp();
+    isPlaying = false
 }); 
